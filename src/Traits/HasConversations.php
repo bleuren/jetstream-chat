de73e8cfe60e00d85a50e64bb82de063d32a28d3
Bleu <bleuren421@gmail.com>
2025-03-18 04:51:16 +0800

    新增 messages 方法以取得使用者的訊息，擴展 HasConversations trait 的功能。

--- src/Traits/HasConversations.php
+++ src/Traits/HasConversations.php
@@ -3,6 +3,7 @@
 namespace Bleuren\JetstreamChat\Traits;
 
 use Bleuren\JetstreamChat\Models\ConversationParticipant;
+use Bleuren\JetstreamChat\Models\Message;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait HasConversations
@@ -13,6 +14,14 @@ trait HasConversations
     public function conversations(): HasMany
     {
         return $this->hasMany(ConversationParticipant::class, 'user_id');
+    }
+
+    /**
+     * 取得使用者的訊息
+     */
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class, 'user_id');
     }
 
     /**
